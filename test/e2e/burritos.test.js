8324dc92166476ec87b9dbc7f847bfaebfe961e0
Kevin Grayson <k.grayson.ms@gmail.com>
2018-07-18 09:33:26 -0700

    Setting up e2e connections and first e2e test

---
+++ test/e2e/burritos.test.js
@@ -0,0 +1,11 @@
+const { assert } = require('chai');
+const request = require('./request');
+// const { dropCollection } = require('./db');
+
+describe('Burritos API', () => {
+
+    it('Saves a burrito', () => {
+        assert.isOk(burrito._id);
+    });
+
+});
