b0944831e4fad54986ae684645bd8baa198b1dca
Kevin Grayson <k.grayson.ms@gmail.com>
2018-07-18 10:18:53 -0700

    Gets a list of burritos test passing

--- test/e2e/burritos.test.js
+++ test/e2e/burritos.test.js
@@ -4,9 +4,10 @@ const { dropCollection } = require('./db');
 
 describe('Burritos API', () => {
 
-    beforeEach(() => dropCollection('foods'));
+    beforeEach(() => dropCollection('burritos'));
 
-    let burrito;
+    let carneAsada;
+    let californiaBurrito;
 
     beforeEach(() => {
         const data = {
@@ -20,11 +21,35 @@ describe('Burritos API', () => {
         return request
             .post('/api/burritos')
             .send(data)
-            .then(({ body }) => burrito = body);
+            .then(({ body }) => carneAsada = body);
+    });
+
+    beforeEach(() => {
+        const data = {
+            name: 'California Burrito',
+            price: 7.00,
+            ingredients: {
+                meat: 'beef',
+                vegetables: 'Onions, French Fries'
+            }
+        };
+
+        return request
+            .post('/api/burritos')
+            .send(data)
+            .then(({ body }) => californiaBurrito = body);
     });
 
     it('Saves a burrito', () => {
-        assert.isOk(burrito._id);
+        assert.isOk(carneAsada._id);
+    });
+
+    it('Gets a list of burritos', () => {
+        return request
+            .get('/api/burritos')
+            .then(({ body }) => {
+                assert.deepEqual(body, [carneAsada, californiaBurrito]);
+            });
     });
 
 });
