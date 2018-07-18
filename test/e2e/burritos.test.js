7792a532464ef5502d334b820a8a760c0250af83
Kevin Grayson <k.grayson.ms@gmail.com>
2018-07-18 09:48:31 -0700

    Burrito route working, saves a burrito test passing

--- test/e2e/burritos.test.js
+++ test/e2e/burritos.test.js
@@ -1,8 +1,27 @@
 const { assert } = require('chai');
 const request = require('./request');
-// const { dropCollection } = require('./db');
+const { dropCollection } = require('./db');
 
 describe('Burritos API', () => {
+
+    beforeEach(() => dropCollection('foods'));
+
+    let burrito;
+
+    beforeEach(() => {
+        const data = {
+            name: 'Carne Asada',
+            price: 5.00,
+            ingredients: {
+                meat: 'beef'
+            }
+        };
+
+        return request
+            .post('/api/burritos')
+            .send(data)
+            .then(({ body }) => burrito = body);
+    });
 
     it('Saves a burrito', () => {
         assert.isOk(burrito._id);
